7db77922c1767a2af48b22e98fceeb893ce08742
yeliena-bochagova <yeliena-bochagova@users.noreply.github.com>
2026-05-19 16:41:18 +0000

    deploy: 750cb7c4d184c242cf72e9b4a9fe62c0f20f2d14

---
+++ plugins/check_toc.py
@@ -0,0 +1,16 @@
+# Context: https://github.com/squidfunk/mkdocs-material/discussions/8252
+
+from mkdocs.plugins import BasePlugin
+
+
+class InvalidPageStructure(Exception):
+    pass
+
+
+class CheckTocPlugin(BasePlugin):
+    def on_page_content(self, html, *, page, config, files):
+        if len(page.toc) > 1:
+            raise InvalidPageStructure(
+                f'Page "{page.url}" has to many #top-level headings ({len(page.toc)}),'
+                " Table Of Contents will be broken"
+            )
